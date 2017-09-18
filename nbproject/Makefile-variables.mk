5ae5aa31905ec501314a39981b9ab664af53a9a4
Sdubos <sebastien.dubos@gmail.com>
2017-09-18 10:52:59 +0200

    origine

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Lora868M.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Lora868M.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lora868m.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lora868m.x.tar
