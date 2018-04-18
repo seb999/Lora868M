d8bdd7bcbab3f3ce28660d721526dcbd50f75edd
seb <sebastien.dubos@gmail.com>
2018-04-18 22:58:41 +0200

    Routine to control LORA : EUART is working

--- main.c
+++ main.c
@@ -28,50 +28,61 @@ void main(void)
 inter_sw1()
 {
     CLRWDT();
-    char res[50]= "";
+    char res[20]="";
+    //res[0] = "\n";
    
     //wait that user release button
     while(SW1_PORT == 0);
     
-     //Read GPS position
-    EUART_GPS();
-    SendUartCmd("$PUBX,40,GLL,1,1,0,0,0\r\n");
-    ReadUartCmd(res);
-    SendUartCmd(res);
-    SendUartCmd("$PUBX,40,GLL,1,0,0,0,0\r\n");
-    CLRWDT();
+     //Read GPS position TESTED
+//    EUART_GPS();
+//    SendUartCmd("$PUBX,40,GLL,1,1,0,0,0\r\n"); //GPS ON
+//    ReadUartCmd(res);
+//    SendUartCmd(res);
+//    SendUartCmd("$PUBX,40,GLL,1,0,0,0,0\r\n");// GPS OFF
+//    CLRWDT();
     
     //Look for LORA NETWORK
     EUART_LORA();
-    SendUartCmd("sys reset\r\n");
+    SendUartCmd("sys reset\r\n"); 
     ReadUartCmd(res);
-    CLRWDT();
+ 
     SendUartCmd("mac join otaa\r\n");
     ReadUartCmd(res);
-    CLRWDT();
-    if(res != "ok") return;
-    LED_GREEN_PORT = 1;
-    __delay_ms(200);
-    LED_GREEN_PORT = 0;
-    __delay_ms(200);
-    LED_GREEN_PORT = 1;
-    __delay_ms(200);
-    LED_GREEN_PORT = 0;
+    SendUartCmd(res);
+    
     ReadUartCmd(res);
-    CLRWDT();
-    if(res != "accepted") return;
-    SendUartCmd("mac tx uncnf 9 123456789\r\n");
-    ReadUartCmd(res);
-    CLRWDT();
-    if(res != "ok") return;
-    LED_GREEN_PORT = 1;
-    __delay_ms(300);
-    LED_GREEN_PORT = 0;
-    __delay_ms(300);
-    LED_GREEN_PORT = 1;
-    __delay_ms(300);
-    LED_GREEN_PORT = 0;
-    CLRWDT();
+    SendUartCmd(res);
+
+    //char ttt2 = ReadUartCmd();
+    //SendUartCmd(ttt2);
+    //res[0] = '\0';
+    //ReadUartCmd(res);
+     //SendUartCmd(res);
+//    CLRWDT();
+//    if(res != "ok") return;
+//    LED_GREEN_PORT = 1;
+//    __delay_ms(200);
+//    LED_GREEN_PORT = 0;
+//    __delay_ms(200);
+//    LED_GREEN_PORT = 1;
+//    __delay_ms(200);
+//    LED_GREEN_PORT = 0;
+//    ReadUartCmd(res);
+//    CLRWDT();
+//    if(res != "accepted") return;
+//    SendUartCmd("mac tx uncnf 9 123456789\r\n");
+//    ReadUartCmd(res);
+//    CLRWDT();
+//    if(res != "ok") return;
+//    LED_GREEN_PORT = 1;
+//    __delay_ms(300);
+//    LED_GREEN_PORT = 0;
+//    __delay_ms(300);
+//    LED_GREEN_PORT = 1;
+//    __delay_ms(300);
+//    LED_GREEN_PORT = 0;
+//    CLRWDT();
 
     IOCBFbits.IOCBF6 = 0;
 }
